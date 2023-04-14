143be119b4a25d0740c40cb9d339d97ca137ccb5
Hiroya Shitogiden <dende.capo@gmail.com>
2023-04-14 17:04:59 +0900

    ローカルストレージをindexedDBへ変更

--- src/globalState/atoms/drafts.tsx
+++ src/globalState/atoms/drafts.tsx
@@ -1,12 +1,41 @@
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 import { draftObject } from "../selector/editorState";
+import localforage from "localforage";
 
 export type draftObjectArray = draftObject[];
 
+localforage.config({
+	driver: localforage.INDEXEDDB, // Use IndexedDB as the backend storage
+	name: "drafts",
+	version: 1,
+	storeName: "draftObjectArray"
+});
+
+const customStorage = () => {
+	return {
+		setItem: (key, value) => {
+			// handle setItem
+			localforage.setItem(key, value);
+			// if err is non-null, we got an error
+		},
+		getItem: (key) => {
+			// handle getItem
+			// this function should return something
+			const a = localforage.getItem(key, function (err, value) {
+				// if err is non-null, we got an error. otherwise, value is the value
+			});
+			return a;
+		},
+		clear: () => {
+			// clear the whole db
+		}
+	};
+};
+
 const { persistAtom } = recoilPersist({
 	key: "recoil-persist",
-	storage: typeof window === "undefined" ? undefined : localStorage
+	storage: typeof window === "undefined" ? undefined : customStorage()
 });
 
 const defaultArray: draftObjectArray = [];
