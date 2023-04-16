6d2b824d1e447a3ee7b09994da0be19f3ec2ed58
dende-h <miyuki_dende@yahoo.co.jp>
2023-04-17 01:39:02 +0900

    localstrageに

--- src/globalState/atoms/drafts.tsx
+++ src/globalState/atoms/drafts.tsx
@@ -1,40 +1,12 @@
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 import { draftObject } from "../selector/editorState";
-import localforage from "localforage";
 
 export type draftObjectArray = draftObject[];
 
-localforage.config({
-	driver: localforage.INDEXEDDB, // Use IndexedDB as the backend storage
-	name: "drafts",
-	version: 1,
-	storeName: "draftObjectArray"
-});
-
-const customStorage = () => {
-	return {
-		setItem: (key, value) => {
-			// handle setItem
-			localforage.setItem(key, value);
-			// if err is non-null, we got an error
-		},
-		getItem: (key) => {
-			// handle getItem
-			// this function should return something
-			return localforage.getItem(key).then((value) => {
-				return value as string;
-			});
-		},
-		clear: () => {
-			// clear the whole db
-		}
-	};
-};
-
 const { persistAtom } = recoilPersist({
 	key: "recoil-persist",
-	storage: typeof window === "undefined" ? undefined : customStorage()
+	storage: typeof window === "undefined" ? undefined : localStorage
 });
 
 const defaultArray: draftObjectArray = [];
