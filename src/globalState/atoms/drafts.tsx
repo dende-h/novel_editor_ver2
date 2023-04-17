0f70f53eff4ec255428e882418b53316907d0a53
Hiroya Shitogiden <dende.capo@gmail.com>
2023-04-17 13:17:09 +0900

    ローカルストレージをindexedDBへ変更の修正

--- src/globalState/atoms/drafts.tsx
+++ src/globalState/atoms/drafts.tsx
@@ -1,31 +1,42 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 import { draftObject } from "../selector/editorState";
 import localforage from "localforage";
+import { PersistStorage } from "recoil-persist";
 
 export type draftObjectArray = draftObject[];
 
 localforage.config({
-	driver: localforage.INDEXEDDB, // Use IndexedDB as the backend storage
+	driver: localforage.INDEXEDDB, // Force WebSQL; same as using setDriver()
 	name: "drafts",
-	version: 1,
-	storeName: "draftObjectArray"
+	version: 1.0,
+	storeName: "draftObject" // Should be alphanumeric, with underscores.
 });
 
-const customStorage = () => {
+interface CustomStorage extends PersistStorage {
+	clear: () => void;
+}
+
+const customStorage = (): CustomStorage => {
 	return {
-		setItem: (key, value) => {
+		setItem: (key: string, value: string) => {
 			// handle setItem
 			localforage.setItem(key, value);
 			// if err is non-null, we got an error
 		},
-		getItem: (key) => {
+		getItem: (key: string) => {
 			// handle getItem
 			// this function should return something
-			const a: Promise<string> = localforage.getItem(key, function (err, value) {
-				// if err is non-null, we got an error. otherwise, value is the value
+			return new Promise<string>((resolve, reject) => {
+				localforage.getItem(key, (err, value: string | null) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(value || "");
+					}
+				});
 			});
-			return a;
 		},
 		clear: () => {
 			// clear the whole db
@@ -35,6 +46,7 @@ const customStorage = () => {
 
 const { persistAtom } = recoilPersist({
 	key: "recoil-persist",
+	// @ts-ignore
 	storage: typeof window === "undefined" ? undefined : customStorage()
 });
 
