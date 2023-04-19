b347afd842303f2d2442546125102d1f4b061dd7
Hiroya Shitogiden <dende.capo@gmail.com>
2023-04-19 16:47:13 +0900

    customRecoilPersist作成

---
+++ src/components/util/customRecoilPersist.tsx
@@ -0,0 +1,111 @@
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import { AtomEffect } from "recoil";
+type PersistItemValue = string | undefined | null;
+export interface PersistStorage {
+	setItem(key: string, value: string): any;
+	mergeItem?(key: string, value: string): any;
+	getItem(key: string): PersistItemValue | Promise<PersistItemValue>;
+}
+export interface PersistState {}
+export interface PersistConfiguration {
+	key: string;
+	storage: PersistStorage;
+}
+
+interface PendingChanges {
+	queue: Promise<any> | null;
+	updates: Partial<PersistState>;
+	reset: Record<string, boolean>;
+}
+
+/**
+ * Recoil module to persist state to storage
+ *
+ * @param config Optional configuration object
+ * @param config.key Used as key in local storage, defaults to `recoil-persist`
+ * @param config.storage Local storage to use, defaults to `localStorage`
+ */
+export const recoilPersist = (config: Partial<PersistConfiguration> = {}): { persistAtom: AtomEffect<any> } => {
+	if (typeof window === "undefined") {
+		return {
+			persistAtom: () => {}
+		};
+	}
+	const {
+		key = "recoil-persist" as PersistConfiguration["key"],
+		storage = localStorage as PersistConfiguration["storage"]
+	} = config;
+
+	const pendingChanges: PendingChanges = {
+		queue: null,
+		updates: {},
+		reset: {}
+	};
+
+	const persistAtom: AtomEffect<any> = ({ onSet, node, trigger, setSelf }) => {
+		if (trigger === "get") {
+			getState().then((s) => {
+				if (s.hasOwnProperty(node.key)) {
+					setSelf(s[node.key]);
+				}
+			});
+		}
+
+		onSet((newValue, _, isReset) => {
+			if (isReset) {
+				pendingChanges.reset[node.key] = true;
+				delete pendingChanges.updates[node.key];
+				console.log(`Reset value for key "${node.key}"`);
+			} else {
+				pendingChanges.updates[node.key] = newValue;
+				console.log(`Updated value for key "${node.key}" to`, newValue);
+			}
+			if (!pendingChanges.queue) {
+				pendingChanges.queue = getState().then((state) => {
+					updateState(state, pendingChanges);
+					pendingChanges.queue = null;
+					pendingChanges.reset = {};
+					pendingChanges.updates = {};
+					console.log(`State persisted to storage with updates:`, pendingChanges.updates);
+				});
+			}
+		});
+	};
+
+	const updateState = (state: PersistState, changes: PendingChanges) => {
+		Object.keys(changes.reset).forEach((key) => {
+			delete state[key];
+		});
+		Object.keys(changes.updates).forEach((key) => {
+			state[key] = changes.updates[key];
+		});
+		setState(state);
+	};
+
+	const parseState = (toParse: PersistItemValue): PersistState => {
+		if (toParse === null || toParse === undefined) {
+			return {};
+		}
+		try {
+			return JSON.parse(toParse);
+		} catch (e) {
+			console.error(e);
+			return {};
+		}
+	};
+	const getState = (): Promise<PersistState> => Promise.resolve(storage.getItem(key)).then(parseState);
+	const setState = (state: PersistState): void => {
+		try {
+			if (typeof storage.mergeItem === "function") {
+				storage.mergeItem(key, JSON.stringify(state));
+			} else {
+				storage.setItem(key, JSON.stringify(state));
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	};
+	return { persistAtom };
+};
--- src/globalState/atoms/drafts.tsx
+++ src/globalState/atoms/drafts.tsx
@@ -1,9 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
+import { recoilPersist } from "../../components/util/customRecoilPersist";
 import { draftObject } from "../selector/editorState";
 import localforage from "localforage";
-import { PersistStorage } from "recoil-persist";
 
 export type draftObjectArray = draftObject[];
 
@@ -14,26 +14,10 @@ localforage.config({
 	storeName: "draftObject"
 });
 
-const customStorage = (): PersistStorage => {
-	return {
-		setItem: async (key: string, value: string) => {
-			console.log(`Saving data for key ${key}: ${value}`);
-			await localforage.setItem(key, value);
-			console.log(`Saved data for key ${key}: ${value}`);
-		},
-		getItem: async (key: string): Promise<string> => {
-			console.log(`Retrieving data for key ${key}`);
-			const value: string = await localforage.getItem(key);
-			console.log(`Retrieved data for key ${key}: ${value}`);
-			return value === null ? undefined : value;
-		}
-	};
-};
-
 const { persistAtom } = recoilPersist({
-	key: "recoil-persist",
+	key: "recoil-indexeddb",
 
-	storage: typeof window === "undefined" ? undefined : customStorage()
+	storage: typeof window === "undefined" ? undefined : localforage
 });
 
 export const drafts = atom({
