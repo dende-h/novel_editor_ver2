ec0d51f03484110e36b99e3a741837644d62624c
Hiroya Shitogiden <dende.capo@gmail.com>
2023-04-24 13:57:08 +0900

    ポリシー変更

--- src/pages/policy.tsx
+++ src/pages/policy.tsx
@@ -4,11 +4,11 @@ export default function PrivacyPolicy() {
 	return (
 		<VStack maxW="800px" mx="auto" px={4} py={8} spacing={8}>
 			<Heading as="h1" size="lg">
-				Re:teratureのプライバシーポリシー
+				Re:terature及びLit:Biteのプライバシーポリシー
 			</Heading>
 			<VStack align="start" spacing={4}>
 				<Text>
-					このドキュメントでは、Re:teratureアプリにおいて個人情報を収集、使用、保護する方法について説明します。
+					このドキュメントでは、Re:terature及びLit:Biteアプリにおいて個人情報を収集、使用、保護する方法について説明します。
 				</Text>
 				<Text fontWeight="bold">1. 収集する情報</Text>
 				<Text>
@@ -36,7 +36,7 @@ export default function PrivacyPolicy() {
 					本サイトは、利用者に事前の通知なく、いつでもプライバシーポリシーを変更することができます。変更後のプライバシーポリシーは、本サイトに掲載した時点で効力を発揮します。
 				</Text>
 				<Text fontWeight="bold">7. 最終更新日</Text>
-				<Text>本プライバシーポリシーは2023年4月17日に最終更新されました。</Text>
+				<Text>本プライバシーポリシーは2023年4月24日に最終更新されました。</Text>
 			</VStack>
 		</VStack>
 	);
