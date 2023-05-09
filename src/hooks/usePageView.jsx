6f0abc143c5d556abc5058accb573064c3abb12a
Hiroya Shitogiden <dende.capo@gmail.com>
2023-05-09 11:38:18 +0900

    スペルミス修正

--- src/hooks/usePageView.jsx
+++ src/hooks/usePageView.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-import { existsGaId, pageview } from "../../lib/gtag";
+import { existsGaId, pageView } from "../../lib/gtag";
 
 export default function usePageView() {
 	const router = useRouter();
@@ -12,7 +12,7 @@ export default function usePageView() {
 		}
 
 		const handleRouteChange = (path) => {
-			pageview(path);
+			pageView(path);
 		};
 
 		router.events.on("routeChangeComplete", handleRouteChange);
