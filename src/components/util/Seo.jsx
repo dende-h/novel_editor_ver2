e99256fcbbd16c008f19d7d5bf485bba108d945c
Hiroya Shitogiden <dende.capo@gmail.com>
2023-05-09 13:50:30 +0900

    metatag順番変更

--- src/components/util/Seo.jsx
+++ src/components/util/Seo.jsx
@@ -20,7 +20,6 @@ const Seo = ({ pageTitle, pageDescription, pagePath, pageImg, pageImgWidth, page
 			<meta name="viewport" content="width=device-width,initial-scale=1.0" />
 			<meta name="description" content={description} />
 			<meta name="twitter:card" content="summary_large_image" />
-			<meta name="twitter:title" content={description} />
 			<meta property="og:url" content={url} />
 			<meta property="og:title" content={title} />
 			<meta property="og:site_name" content={title} />
@@ -29,6 +28,7 @@ const Seo = ({ pageTitle, pageDescription, pagePath, pageImg, pageImgWidth, page
 			<meta property="og:image" content={imgUrl} />
 			<meta property="og:image:width" content={String(imgWidth)} />
 			<meta property="og:image:height" content={String(imgHeight)} />
+			<meta name="twitter:title" content={description} />
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link rel="canonical" href={url} />
 			<link rel="icon" href="/favicon.ico" />
