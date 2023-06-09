2bd1c13600791cdbed85787dde860cfcfbc5dade
Hiroya Shitogiden <dende.capo@gmail.com>
2023-06-09 13:41:14 +0900

    エンドポイントコード修正

--- src/pages/api/downloadtext.ts
+++ src/pages/api/downloadtext.ts
@@ -1,21 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-const downloadText = async (req: NextApiRequest, res: NextApiResponse) => {
+const downloadHandler = (req: NextApiRequest, res: NextApiResponse) => {
 	try {
-		const { title, text } = req.query;
-		if (typeof title !== "string" || typeof text !== "string") {
-			res.status(400).json({ message: "Invalid query parameters" });
-			return;
-		}
-
-		const encodedTitle = encodeURIComponent(title);
+		const fileName = req.query.title as string;
+		const text = req.query.text as string;
+		const encodedTitle = encodeURIComponent(fileName);
+		res.setHeader("Content-Disposition", `attachment; filename=${encodedTitle}.txt`);
 		res.setHeader("Content-Type", "text/plain");
-		res.setHeader("Content-Disposition", `attachment; filename="${encodedTitle}.txt"`);
-		res.send(text);
+		res.status(200).send(text);
 	} catch (error) {
 		console.error("Error in downloadText:", error);
 		res.status(500).json({ message: "Internal server error" });
 	}
 };
 
-export default downloadText;
+export default downloadHandler;
