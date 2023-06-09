4b9da51d63697f4dec9d008922620c8de5857166
Hiroya Shitogiden <dende.capo@gmail.com>
2023-06-09 13:20:56 +0900

    ファイルタイトルのエンコード追加

--- src/pages/api/downloadtext.ts
+++ src/pages/api/downloadtext.ts
@@ -1,12 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-const downloadHandler = (req: NextApiRequest, res: NextApiResponse) => {
-	const fileName = req.query.title as string;
-	const text = req.query.text as string;
+const downloadText = async (req: NextApiRequest, res: NextApiResponse) => {
+	try {
+		const { title, text } = req.query;
+		if (typeof title !== "string" || typeof text !== "string") {
+			res.status(400).json({ message: "Invalid query parameters" });
+			return;
+		}
 
-	res.setHeader("Content-Disposition", `attachment; filename=${fileName}.txt`);
-	res.setHeader("Content-Type", "text/plain");
-	res.status(200).send(text);
+		const encodedTitle = encodeURIComponent(title);
+		res.setHeader("Content-Type", "text/plain");
+		res.setHeader("Content-Disposition", `attachment; filename="${encodedTitle}.txt"`);
+		res.send(text);
+	} catch (error) {
+		console.error("Error in downloadText:", error);
+		res.status(500).json({ message: "Internal server error" });
+	}
 };
 
-export default downloadHandler;
+export default downloadText;
