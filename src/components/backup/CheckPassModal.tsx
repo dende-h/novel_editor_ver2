f7768f19cfc951b61bfa4afbf5cb62052a350d46
Hiroya Shitogiden <dende.capo@gmail.com>
2023-06-12 17:12:41 +0900

    passワードチェック実装

---
+++ src/components/backup/CheckPassModal.tsx
@@ -0,0 +1,115 @@
+import {
+	useDisclosure,
+	Button,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalCloseButton,
+	ModalBody,
+	ModalFooter,
+	Input,
+	HStack,
+	Center,
+	Text,
+	useColorModeValue,
+	FormControl,
+	FormErrorMessage
+} from "@chakra-ui/react";
+import { memo, useEffect, useState } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { passWord } from "../../globalState/atoms/passWord";
+import { useCalcCharCount } from "../../hooks/useCalcCharCount";
+import { useInput } from "../../hooks/useInput";
+import { hash, compare } from "bcryptjs";
+import { ChangePassWordModal } from "./ChangePassWordModal";
+
+export const CheckPassWordModal = memo(() => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const backgroundColor = useColorModeValue("gray.200", "gray.600");
+	const inputfontColor = useColorModeValue("gray.700", "gray.700");
+	const buttonHoverBgColor = useColorModeValue("gray.300", "gray.500");
+	const setPass = useSetRecoilState(passWord);
+	const { onChangeInputForm, value, setValue } = useInput();
+	const { calcCharCount, charCount } = useCalcCharCount();
+	const pass = useRecoilValue(passWord);
+	const maxLength = 15;
+
+	const [isValid, setIsValid] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const onChangeInputFormWithValidation = async (e) => {
+		onChangeInputForm(e);
+		const value: string = e.target.value;
+		const isValid = value.includes(pass);
+
+		setIsValid(isValid);
+
+		if (!isValid) {
+			setErrorMessage("現在のパスワードと一致しません");
+		} else {
+			setErrorMessage("");
+		}
+	};
+
+	const onCloseModal = () => {
+		setValue("");
+		onClose();
+	};
+
+	useEffect(() => {
+		calcCharCount(value);
+	}, [value]);
+
+	return (
+		<>
+			<Button
+				colorScheme={"teal"}
+				onClick={onOpen}
+				size={{ base: "xs", md: "sm", lg: "md" }}
+				fontSize={{ base: "xs", md: "sm", lg: "lg" }}
+				margin={1}
+			>
+				パスワードの変更
+			</Button>
+			<Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onCloseModal} size={"3xl"}>
+				<ModalOverlay />
+				<ModalContent backgroundColor={backgroundColor} borderRadius={"md"} border={"1px"} boxShadow={"lg"}>
+					<ModalHeader fontSize={"lg"} fontWeight={"bold"}>
+						現在のパスワード確認
+					</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody pb={6} paddingTop={"0"}>
+						<Center padding={2}>
+							<HStack>
+								<FormControl isInvalid={!isValid && charCount > 0}>
+									<Input
+										_focus={{ backgroundColor: isValid ? "green.100" : "red.100", boxShadow: "none" }}
+										placeholder={"現在のパスワードを入力してください"}
+										onChange={onChangeInputFormWithValidation}
+										maxLength={maxLength}
+										w={"300px"}
+										overflow={"hidden"}
+										borderColor={isValid ? "green.500" : "red.500"}
+										color={inputfontColor}
+									/>
+									<FormErrorMessage>{errorMessage}</FormErrorMessage>
+								</FormControl>
+								<Text>
+									{charCount}/{maxLength}
+								</Text>
+							</HStack>
+						</Center>
+					</ModalBody>
+					<ModalFooter>
+						{isValid && <ChangePassWordModal />}
+						<Button onClick={onCloseModal} variant={"ghost"} _hover={{ bg: buttonHoverBgColor }}>
+							キャンセル
+						</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	);
+});
+CheckPassWordModal.displayName = "CheckPassWordModal";
--- src/pages/backup.tsx
+++ src/pages/backup.tsx
@@ -8,7 +8,8 @@ import {
 	HStack,
 	Spacer,
 	StackDivider,
-	Divider
+	Divider,
+	Spinner
 } from "@chakra-ui/react";
 import format from "date-fns/format";
 import { useEffect, useState } from "react";
@@ -34,6 +35,7 @@ import { AlertDialogBackUpReconstruction } from "../components/backup/AlertDialo
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { memoState } from "../globalState/atoms/memoState";
 import { hash, compare } from "bcryptjs";
+import { CheckPassWordModal } from "../components/backup/CheckPassModal";
 
 export default function BackUP() {
 	const setDrafts = useSetRecoilState(drafts);
@@ -161,37 +163,17 @@ export default function BackUP() {
 						<Box textAlign={"end"}>{isPublished ? undefined : <ChangeUserNameModal />}</Box>
 					</Box>
 					<Box>
-						<HStack>
-							<Text fontSize="lg" fontWeight="bold" color={textColor}>
-								バックアップ用パスワード
-							</Text>
-							{showPass ? (
-								<ViewOffIcon onClick={() => setShowPass(false)} />
-							) : (
-								<ViewIcon
-									onClick={() => {
-										setShowPass(true);
-									}}
-								/>
-							)}
-						</HStack>
-
+						<Text fontSize="lg" fontWeight="bold" color={textColor}>
+							バックアップ用パスワード
+						</Text>
 						<Box bg={boxColor} p={4} borderRadius="md" shadow="md" minW={"300px"}>
-							<Text fontSize="lg" fontWeight="bold" color={textColor} textAlign="center">
-								{pass
-									? showPass
-										? pass
-										: [...pass].map((_, index) => (
-												<Box as={"span"} key={index} marginRight={"-0.8"}>
-													●
-												</Box>
-										  ))
-									: "passWordを設定してください"}
+							<Text fontSize="lg" fontWeight="bold" color={pass ? "teal.500" : "red.500"} textAlign="center">
+								{pass ? "パスワード設定済み" : "パスワード未設定"}
 							</Text>
 						</Box>
 
 						<Box textAlign={"end"}>
-							<ChangePassWordModal />
+							{isLoading ? <Spinner /> : pass === null ? <ChangePassWordModal /> : <CheckPassWordModal />}
 						</Box>
 						<Text fontSize={"11px"} color="red.500">
 							※パスワードを忘れた場合復元が出来なくなります。必ず控えを取っておいてください
