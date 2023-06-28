9d24584a8cd74e04bb7f487c89f6f84ac6c3a007
Hiroya Shitogiden <dende.capo@gmail.com>
2023-06-28 17:32:40 +0900

    前書き後書き記入欄追加

--- src/components/editorArea/addPrefaceAndPostscript/AddPrefaceAndPostscript.tsx
+++ src/components/editorArea/addPrefaceAndPostscript/AddPrefaceAndPostscript.tsx
@@ -9,7 +9,8 @@ import {
 	useColorModeValue,
 	Button,
 	DrawerFooter,
-	Textarea
+	Textarea,
+	VStack
 } from "@chakra-ui/react";
 import { LegacyRef, memo, useRef, useState } from "react";
 import { useDraft } from "../../../hooks/useDraft";
@@ -23,6 +24,7 @@ export const AddPrefaceAndPostscript = memo(({ defaultPreface, defaultPostscript
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const btnRef: LegacyRef<HTMLButtonElement> = useRef();
 	const backgroundColor = useColorModeValue("gray.200", "gray.600");
+	const bgTextArea = useColorModeValue("gray.100", "gray.500");
 	const [prefaceText, setPrefaceText] = useState<string>(defaultPreface ? defaultPreface : "");
 	const [postscriptText, setPostscriptText] = useState<string>(defaultPostscript ? defaultPostscript : "");
 	const [isLoading, setIsLoading] = useState(false);
@@ -58,25 +60,31 @@ export const AddPrefaceAndPostscript = memo(({ defaultPreface, defaultPostscript
 				<DrawerOverlay />
 				<DrawerContent sx={css} bgColor={backgroundColor}>
 					<DrawerCloseButton />
-					<DrawerHeader>前書き/後書きの追加</DrawerHeader>
+					<DrawerHeader>【 前書き / 後書き 】</DrawerHeader>
 					<DrawerBody>
-						<Textarea
-							placeholder="前書きを記入できます(200文字)"
-							value={prefaceText}
-							onChange={(e) => {
-								setPrefaceText(e.target.value);
-							}}
-							maxLength={200}
-						></Textarea>
+						<VStack spacing={6}>
+							<Textarea
+								bgColor={bgTextArea}
+								placeholder="前書きやあらすじを記入できます(200文字)"
+								value={prefaceText}
+								onChange={(e) => {
+									setPrefaceText(e.target.value);
+								}}
+								maxLength={200}
+								minH={"300px"}
+							/>
 
-						<Textarea
-							placeholder="後書きを記入できます(200文字)"
-							value={postscriptText}
-							onChange={(e) => {
-								setPostscriptText(e.target.value);
-							}}
-							maxLength={200}
-						></Textarea>
+							<Textarea
+								bgColor={bgTextArea}
+								placeholder="後書きを記入できます(200文字)"
+								value={postscriptText}
+								onChange={(e) => {
+									setPostscriptText(e.target.value);
+								}}
+								maxLength={200}
+								minH={"300px"}
+							/>
+						</VStack>
 					</DrawerBody>
 
 					<DrawerFooter>
