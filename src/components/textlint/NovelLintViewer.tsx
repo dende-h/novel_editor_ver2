09d33823f0f59db2564033e2ab5b114c652a44fb
Hiroya Shitogiden <dende.capo@gmail.com>
2023-07-07 16:43:45 +0900

    アンダーバーと行番号追加

--- src/components/textlint/NovelLintViewer.tsx
+++ src/components/textlint/NovelLintViewer.tsx
@@ -8,24 +8,72 @@ type Props = {
 	result?: Array<any>;
 };
 
+function convertGlobalPosToLocalPos(text: string, globalStartPos: number, globalEndPos: number) {
+	const lines = text.split("\n");
+
+	let currentGlobalPos = 0;
+	for (let i = 0; i < lines.length; i++) {
+		const line = lines[i];
+
+		if (currentGlobalPos + line.length >= globalStartPos) {
+			const localStartPos = globalStartPos - currentGlobalPos;
+			const localEndPos = Math.min(globalEndPos - currentGlobalPos, line.length);
+
+			return { line: i + 1, start: localStartPos, end: localEndPos };
+		}
+
+		currentGlobalPos += line.length + 1; // 1 for newline
+	}
+
+	return null;
+}
+
+function addErrorHighlight(line: string, ranges: Array<{ start: number; end: number }>) {
+	ranges.sort((a, b) => a.start - b.start);
+
+	let highlightedLine = "";
+	let currentIndex = 0;
+
+	for (const { start, end } of ranges) {
+		highlightedLine +=
+			line.substring(currentIndex, start) + "<span style='color: red;'>" + line.substring(start, end) + "</span>";
+		currentIndex = end;
+	}
+
+	highlightedLine += line.substring(currentIndex);
+
+	return highlightedLine;
+}
+
 export const NovelLintViewer: FC<Props> = ({ text, result }) => {
 	const lines = text.split("\n");
-	const lineErrorMap = new Map();
+	const errorRangesPerLine = new Map<number, Array<{ start: number; end: number }>>();
 
 	if (result && Array.isArray(result) && result.length > 0) {
 		for (const item of result) {
-			const { line } = item;
-			if (typeof line === "number") {
-				lineErrorMap.set(line, true);
+			const { range } = item;
+			if (Array.isArray(range) && range.length === 2) {
+				const [globalStartPos, globalEndPos] = range;
+				const localPos = convertGlobalPosToLocalPos(text, globalStartPos, globalEndPos);
+
+				if (localPos !== null) {
+					const { line, start, end } = localPos;
+					if (!errorRangesPerLine.has(line)) {
+						errorRangesPerLine.set(line, []);
+					}
+					errorRangesPerLine.get(line).push({ start, end });
+				}
 			}
 		}
 	}
 
 	const markedLines = lines.map((line, index) => {
-		if (lineErrorMap.has(index + 1)) {
-			return `<p style='color: red;'>${line}</p>`;
+		if (errorRangesPerLine.has(index + 1)) {
+			return `<span style='text-decoration: underline;'><strong style='color: blue;'>${
+				index + 1
+			}:</strong> ${addErrorHighlight(line, errorRangesPerLine.get(index + 1))}</span>`;
 		} else {
-			return line;
+			return `<strong style='color: blue;'>${index + 1}:</strong> ${line}`;
 		}
 	});
 
