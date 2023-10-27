f83a32a605d7bab08345fb4da8a591494d01332c
Hiroya Shitogiden <miyuki_dende@yahoo.co.jp>
2023-10-27 14:05:03 +0900

    コメント追加

--- src/hooks/useInitialJson.tsx
+++ src/hooks/useInitialJson.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { editorJson } from "../globalState/selector/editorJson";
 
+//表示するJsonデータを初期値としてRecoilからセットする
 export const useInitialJson = () => {
 	const json = useRecoilValue(editorJson);
 	const [initialJson, setInitialJson] = useState(json !== null ? json?.json : null);
